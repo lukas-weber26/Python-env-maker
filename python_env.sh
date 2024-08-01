60ee6ace3eeee53117ceb87552493796cd659e3a
Lukas <lukas.weber26@gmail.com>
2024-08-01 16:35:17 +0000

    Fixed the order a bit

--- python_env.sh
+++ python_env.sh
@@ -1,9 +1,10 @@
 #! /bin/bash
-python -m pip install --upgrade pip >> /dev/null 
 python -m venv .
+source ./bin/activate
+python -m pip install --upgrade pip >> /dev/null 
 pip install numpy > env_create_output.txt 
 pip install pandas >> env_create_output.txt 
-source ./bin/activate
+
 
 
 
